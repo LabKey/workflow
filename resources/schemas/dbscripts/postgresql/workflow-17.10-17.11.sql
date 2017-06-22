ea9af4bb20ff081b87789055686a6075e4f66097
labkey-eyounske <eyounske@labkey.com>
2017-06-22 12:44:20 -0700

    17.2 Update all copyrights & licenses   -- .groovy files now included

--- resources/schemas/dbscripts/postgresql/workflow-17.10-17.11.sql
+++ resources/schemas/dbscripts/postgresql/workflow-17.10-17.11.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2017 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 -- Change the resource and diagram resource names from the old form of an absolute filepath to new form of moduleName:filename
 update workflow.ACT_RE_PROCDEF
 set resource_name_ = right(category_, strpos(reverse(category_), ':') - 1) || ':' ||
