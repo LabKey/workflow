ab88f8d7afe6d462d1dee6caf43eae7c1059a36b
Adam Rauch <adam@labkey.com>
2020-01-24 12:23:11 -0800

    Remove "obsolete" directories and scripts

--- resources/schemas/dbscripts/postgresql/obsolete/workflow-16.10-16.11.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Activiti BPM Platform
- * Copyright 2010-2016 Alfresco Software, Ltd.
- *
- * Licensed under the Apache License, Version 2.0- http://www.apache.org/licenses/LICENSE-2.0
- */
-DELETE FROM workflow.act_ru_variable WHERE name_ = 'processInstanceUrl';
-DELETE FROM workflow.act_hi_varinst WHERE name_ = 'processInstanceUrl';
-DELETE FROM workflow.act_ge_bytearray WHERE name_ LIKE '%var-processInstanceUrl';
