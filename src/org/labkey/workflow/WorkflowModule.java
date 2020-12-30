51b76ddbb1bc4464636cdf1a0565256400b1f013
Adam Rauch <adam@labkey.com>
2020-12-30 07:14:36 -0800

    Schema bump to 21.000 (#50)

--- src/org/labkey/workflow/WorkflowModule.java
+++ src/org/labkey/workflow/WorkflowModule.java
@@ -45,7 +45,7 @@ public class WorkflowModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 20.000;
+        return 21.000;
     }
 
     @Override
